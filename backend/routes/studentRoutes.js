681e4bda675a6faa312d83bfba856c0798625ac3
elabaidoumniya <elabaid.oumniya@etu.uae.ac.ma>
2025-02-25 18:23:05 +0000

    Update student routes

--- backend/routes/studentRoutes.js
+++ backend/routes/studentRoutes.js
@@ -21,7 +21,53 @@ router.get('/student', async (req, res) => {
         res.status(500).json({ error: 'Failed to retrieve students' });
     }
 })
+router.delete('/student/:id', async (req, res) => {
+    const { id } = req.params;
 
+    try {
+        const result = await pool.query( 'DELETE FROM student WHERE id = $1 RETURNING *', [id]);
+           
+        if (result.rows.length > 0) {
+            res.json({ message: 'Student deleted successfully' });
+        } else {
+            res.status(404).json({ error: 'Student not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to delete student' });
+        console.log(error);
+    }
+});
+
+router.put('/student/:id', async (req, res) => {
+    const { id } = req.params;  
+    const { nom, prenom, note } = req.body;  
+    try {
+        const result = await pool.query('UPDATE student SET nom = $1, prenom = $2, note = $3 WHERE id = $4 RETURNING *',
+            [nom, prenom, note, id]);
+        res.json(result.rows[0]);
+       
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to update student' });
+        console.log(error);
+    }
+});
+
+
+router.get('/student/:id', async (req, res) => {
+    const { id } = req.params;  // Get student ID from URL parameter
+
+    try {
+        const result = await pool.query('SELECT * FROM student WHERE id = $1', [id]);
+        if (result.rows.length > 0) {
+            res.json(result.rows[0]);
+        } else {
+            res.status(404).json({ error: 'Student not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to retrieve student' });
+        console.log(error);
+    }
+});
 /* 
 router.get('/student', studentControllers.getAllStudents)
 router.get('/student/:id', studentControllers.getStudentById)
