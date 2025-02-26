af6b58651ad8785a73d5d2a6da76a842ef4f6140
nawarelhaouat <nawarelhaouat@gmail.com>
2025-02-26 15:28:57 +0000

    add all student endpoint

--- backend/src/controllers/studentController.js
+++ backend/src/controllers/studentController.js
@@ -1,10 +1,11 @@
-import {getAllStudentsService, getStudentByIdService, addStudentService} from '../models/studentModel.js'
+import {getAllStudentsService, getStudentByIdService, addStudentService, updateStudentService, deleteStudentService} from '../models/studentModel.js'
 // Standarized response function
 const handleResponse = (res, status, message, data = null) => {
     status,
     message,
     data
 }
+
 export const getAllStudents = async (req, res, next) => {
     try {
         const result = await getAllStudentsService()
@@ -28,6 +29,27 @@ export const addStudent = async (req, res, next) => {
     try {
         const result = await addStudentService(nom, prenom, note)
         handleResponse(res, 201, 'Student added successfully', result)
+    } catch (err) {
+        next(err)
+    }
+}
+export const updateStudent = async (req, res, next) => {
+    const { id } = req.params
+    const { nom, prenom, note } = req.body
+    try {
+        const result = await updateStudentService(id, nom, prenom, note)
+        if (!result) return handleResponse(res, 404, 'Student not found')
+        handleResponse(res, 200, 'Student updated successfully', result)
+    } catch (err) {
+        next(err)
+    }
+}
+export const deleteStudent = async (req, res, next) => {
+    const { id } = req.params
+    try {
+        const result = await deleteStudentService(id)
+        if (!result) return handleResponse(res, 404, 'Student not found')
+        handleResponse(res, 200, 'Student deleted successfully', result)
     } catch (err) {
         next(err)
     }
--- backend/src/routes/studentRoutes.js
+++ backend/src/routes/studentRoutes.js
@@ -1,81 +1,13 @@
 import express from 'express'
 const router = express.Router()
-import {getAllStudents, getStudentById, addStudent} from '../controllers/studentController.js'
+import {getAllStudents, getStudentById, addStudent,updateStudent , deleteStudent} from '../controllers/studentController.js'
 
 router.get('/', getAllStudents)
 router.get('/:id', getStudentById)
 router.post('/', addStudent)
-/* router.put('/:id', studentControllers.editStudentInfo)
-router.delete('/:id', studentControllers.deleteStudent)
- */
+router.put('/:id', updateStudent)
+router.delete('/:id', deleteStudent)
 
-
-/* router.post('/student', async (req, res) => {
-    const {nom, prenom, note} = req.body
-    try {
-    const result = await pool.query('INSERT INTO student (nom, prenom, note) VALUES ($1, $2, $3) RETURNING *', [nom, prenom, note])
-    res.status(201).json(result.rows[0]);
-} catch (error) {
-    res.status(500).json({ error: 'Failed to add student'})
-    console.log(error)
-}
-})
-router.get('/student', async (req, res) => {
-    try {
-        const result = await pool.query('SELECT * FROM student')
-        res.json(result.rows)
-    } catch (error) {
-        res.status(500).json({ error: 'Failed to retrieve students' });
-    }
-})
-router.delete('/student/:id', async (req, res) => {
-    const { id } = req.params;
-
-    try {
-        const result = await pool.query( 'DELETE FROM student WHERE id = $1 RETURNING *', [id]);
-           
-        if (result.rows.length > 0) {
-            res.json({ message: 'Student deleted successfully' });
-        } else {
-            res.status(404).json({ error: 'Student not found' });
-        }
-    } catch (error) {
-        res.status(500).json({ error: 'Failed to delete student' });
-        console.log(error);
-    }
-});
-
-router.put('/student/:id', async (req, res) => {
-    const { id } = req.params;  
-    const { nom, prenom, note } = req.body;  
-    try {
-        const result = await pool.query('UPDATE student SET nom = $1, prenom = $2, note = $3 WHERE id = $4 RETURNING *',
-            [nom, prenom, note, id]);
-        res.json(result.rows[0]);
-       
-    } catch (error) {
-        res.status(500).json({ error: 'Failed to update student' });
-        console.log(error);
-    }
-});
-
-
-router.get('/student/:id', async (req, res) => {
-    const { id } = req.params;  // Get student ID from URL parameter
-
-    try {
-        const result = await pool.query('SELECT * FROM student WHERE id = $1', [id]);
-        if (result.rows.length > 0) {
-            res.json(result.rows[0]);
-        } else {
-            res.status(404).json({ error: 'Student not found' });
-        }
-    } catch (error) {
-        res.status(500).json({ error: 'Failed to retrieve student' });
-        console.log(error);
-    }
-});
- */
 
 export default router;
 
