cd661da31874d2e7693067a50abc3da4172de2f9
nawarelhaouat <nawarelhaouat@gmail.com>
2025-03-06 22:07:51 +0000

    add confirm password to userSchema

--- backend/src/middlewares/inputValidator.js
+++ backend/src/middlewares/inputValidator.js
@@ -5,6 +5,7 @@ const userSchema = Joi.object({
     prenom: Joi.string().min(3).required(),
     email: Joi.string().email().required(),
     password: Joi.string().min(6).required(),
+    confirmPassword: Joi.string().valid(Joi.ref('password')).required().strict()
 })
 const loginSchema = Joi.object({
     email: Joi.string().email().required(),
