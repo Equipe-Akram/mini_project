3abd39d1952e26dcd34d2db303aabe144380a46a
nawarelhaouat <nawarelhaouat@gmail.com>
2025-03-07 20:05:33 +0000

    remove sql

--- backend/sql/V3__create_refresh_token_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE refresh_token (
-    id SERIAL PRIMARY KEY,
-    professor_id INT REFERENCES professor(id) ON DELETE CASCADE,
-    token TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW()
-);
